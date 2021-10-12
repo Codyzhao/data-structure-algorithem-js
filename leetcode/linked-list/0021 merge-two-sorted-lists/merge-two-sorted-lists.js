e175c38ebc09b944bdc2f795e396a76abd8f78c4
Cody Zhao <zhaocody@gmail.com>
2021-10-13 00:14:00 +1300

    feat: linked list - merge two sorted lists

---
+++ leetcode/linked-list/0021 merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
+
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (l1, l2) {
+  if (l1 === null) {
+    return l2;
+  }
+
+  if (l2 === null) {
+    return l1;
+  }
+
+  if (l1.val < l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2);
+    return l1;
+  }
+
+  l2.next = mergeTwoLists(l1, l2.next);
+  return l2;
+};
+
+var mergeTwoLists2 = function (l1, l2) {
+  let prev = new ListNode(-1);
+  let curr = prev;
+
+  while (l1 !== null && l2 !== null) {
+    if (l1.val < l2.val) {
+      curr.next = l1;
+      l1 = l1.next;
+    } else {
+      curr.next = l2;
+      l2 = l2.next;
+    }
+    curr = curr.next;
+  }
+  curr.next = l1 || l2;
+  return prev.next;
+};
