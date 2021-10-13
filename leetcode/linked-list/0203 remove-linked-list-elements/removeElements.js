c4c033492b9cf86c1ef49b3f8daa3f4ed8829ff5
Cody Zhao <zhaocody@gmail.com>
2021-10-13 20:57:01 +1300

    feat: linked list - remove linked list elements

---
+++ leetcode/linked-list/0203 remove-linked-list-elements/removeElements.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function (head, val) {
+  if (head === null) return head;
+
+  let prev = new ListNode(-1);
+  let curr = prev;
+  curr.next = head;
+  while (curr.next !== null) {
+    if (curr.next === val) {
+      curr.next = curr.next.next;
+    } else {
+      curr = curr.next;
+    }
+  }
+
+  return prev.next;
+};
