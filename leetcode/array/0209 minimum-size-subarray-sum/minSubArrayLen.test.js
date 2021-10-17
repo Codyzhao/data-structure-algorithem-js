d9e3c1ac3f9dc54645465e83d383566769db8913
Cody Zhao <zhaocody@gmail.com>
2021-10-18 11:28:19 +1300

    feat: array - minimun size subarry sum

--- leetcode/array/0209 minimum-size-subarray-sum/minSubArrayLen.test.js
+++ leetcode/array/0209 minimum-size-subarray-sum/minSubArrayLen.test.js
@@ -2,7 +2,7 @@ const minSubArrayLen = require('./minSubArrayLen');
 
 test('minSubArrayLen', () => {
   expect(minSubArrayLen(7, [2, 3, 1, 2, 4, 3])).toEqual(2);
-  // expect(minSubArrayLen(4, [1, 4, 4])).toEqual(1);
-  // expect(minSubArrayLen(11, [1, 1, 1, 1, 1, 1, 1, 1])).toEqual(0);
-  // expect(minSubArrayLen(11, [1, 2, 3, 4, 5])).toEqual(3);
+  expect(minSubArrayLen(4, [1, 4, 4])).toEqual(1);
+  expect(minSubArrayLen(11, [1, 1, 1, 1, 1, 1, 1, 1])).toEqual(0);
+  expect(minSubArrayLen(11, [1, 2, 3, 4, 5])).toEqual(3);
 });
