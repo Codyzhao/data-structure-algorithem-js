ebac30b26451dda6023a2624d663ca800bac719f
Cody Zhao <zhaocody@gmail.com>
2021-10-17 21:28:45 +1300

    feat: two-pointer - reverse string

--- leetcode/array/0026 remove-duplicates/remove-duplicates.js
+++ leetcode/array/0026 remove-duplicates/remove-duplicates.js
@@ -1,4 +1,4 @@
-const removeDuplicates = (nums) => {
+const removeDuplicates1 = (nums) => {
   if (nums.length === 0) return 0;
   let last = 0;
   let finder = 0;
@@ -16,6 +16,23 @@ const removeDuplicates = (nums) => {
   }
 
   return last + 1;
+};
+
+const removeDuplicates = (nums) => {
+  if (nums.length === 0) return 0;
+  let i = 0,
+    j = 0;
+  while (j < nums.length) {
+    if (i === 0 || nums[i - 1] !== nums[j]) {
+      nums[i] = nums[j];
+      i++;
+      j++;
+    } else {
+      j++;
+    }
+  }
+
+  return nums.slice(0, i).length;
 };
 
 module.exports = removeDuplicates;
