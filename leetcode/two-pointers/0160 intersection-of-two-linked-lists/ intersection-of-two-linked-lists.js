25ce787bab009221153c7c3878be5b3fe1a3a9b2
Cody Zhao <zhaocody@gmail.com>
2021-10-24 20:49:47 +1300

    feat: linked list - detect cycle

--- leetcode/two-pointers/0160 intersection-of-two-linked-lists/ intersection-of-two-linked-lists.js
+++ leetcode/two-pointers/0160 intersection-of-two-linked-lists/ intersection-of-two-linked-lists.js
@@ -26,3 +26,33 @@ var getIntersectionNode = function (headA, headB) {
 
   return a;
 };
+
+var getIntersectionNode2 = function (headA, headB) {
+  var getListLen = function (head) {
+    let len = 0,
+      cur = head;
+    while (cur) {
+      len++;
+      cur = cur.next;
+    }
+    return len;
+  };
+
+  let curA = headA,
+    curB = headB,
+    lenA = getListLen(headA),
+    lenB = getListLen(headB);
+  if (lenA < lenB) {
+    [curA, curB] = [curB, curA];
+    [lenA, lenB] = [lenB, lenA];
+  }
+  let i = lenA - lenB;
+  while (i-- > 0) {
+    curA = curA.next;
+  }
+  while (curA && curA !== curB) {
+    curA = curA.next;
+    curB = curB.next;
+  }
+  return curA;
+};
