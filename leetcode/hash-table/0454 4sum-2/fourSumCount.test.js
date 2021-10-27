2af7d78a1d197567ce4a4b15672105ba7ea4dece
Cody Zhao <zhaocody@gmail.com>
2021-10-27 21:04:39 +1300

    feat: hash table - four sum 2

---
+++ leetcode/hash-table/0454 4sum-2/fourSumCount.test.js
@@ -0,0 +1,7 @@
+const { expect } = require('@jest/globals');
+const fourSumCount = require('./fourSumCount');
+
+test('fourSumCount', () => {
+  expect(fourSumCount([1, 2], [-2, -1], [-1, 2], [0, 2])).toEqual(2);
+  expect(fourSumCount([0], [0], [0], [0])).toEqual(1);
+});
--- leetcode/two-pointers/0202 happy-number/happy-number.js
+++ leetcode/two-pointers/0202 happy-number/happy-number.js
@@ -1,4 +1,4 @@
-const isHappy = (n) => {
+const isHappy1 = (n) => {
   if (n < 0) return false;
   const seen = new Map();
 
@@ -18,5 +18,38 @@ function getSqr(num) {
   }
   return res;
 }
+
+function getN(n) {
+  if (n === 1 || n === 0) {
+    return n;
+  }
+  let res = 0;
+  while (n) {
+    res += (n % 10) * (n % 10);
+    n = parseInt(n / 10);
+  }
+  return res;
+}
+
+const isHappy2 = (n) => {
+  const sumSet = new Set();
+  while (n !== 1 && !sumSet.has(n)) {
+    sumSet.add(n);
+    n = getN(n);
+  }
+  return n === 1;
+};
+
+const isHappy = (n) => {
+  if (getN(n) == 1) return true;
+  let a = getN(n),
+    b = getN(getN(n));
+  // 如果 a === b
+  while (b !== 1 && getN(b) !== 1 && a !== b) {
+    a = getN(a);
+    b = getN(getN(b));
+  }
+  return b === 1 || getN(b) === 1;
+};
 
 module.exports = isHappy;
