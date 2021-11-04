64c194e997d58a7aa62d099234891fdc08d017a8
Cody Zhao <zhaocody@gmail.com>
2021-11-04 20:26:21 +1300

    feat: string - strStr

--- leetcode/string/0028 str-str/str-str.js
+++ leetcode/string/0028 str-str/str-str.js
@@ -1,4 +1,4 @@
-const strStr = (haystack, needle) => {
+const strStr1 = (haystack, needle) => {
   const h = haystack.length;
   const n = needle.length;
   if (n === 0) return 0;
@@ -47,5 +47,45 @@ const strStr = (haystack, needle) => {
 
 //   return -1;
 // };
+
+const strStr = (haystack, needle) => {
+  if (needle.length === 0) return 0;
+
+  const getNext = (needle) => {
+    let next = [];
+    let j = -1;
+    next.push(j);
+
+    for (let i = 1; i < needle.length; i++) {
+      while (j >= 0 && needle[i] != needle[j + 1]) {
+        j = next[j];
+      }
+      if (needle[i] === needle[j + 1]) {
+        j++;
+      }
+      next.push(j);
+    }
+    return next;
+  };
+
+  let next = getNext(needle);
+  let j = -1;
+
+  for (let i = 0; i < haystack.length; i++) {
+    while (j >= 0 && haystack[i] !== needle[j + 1]) {
+      j = next[j];
+    }
+
+    if (haystack[i] === needle[j + 1]) {
+      j++;
+    }
+
+    if (j === needle.length - 1) {
+      return i - needle.length + 1;
+    }
+  }
+
+  return -1;
+};
 
 module.exports = strStr;
