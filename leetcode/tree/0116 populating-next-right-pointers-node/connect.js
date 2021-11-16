2651973bbbcd0624555589748a7af2bf71301d08
Cody Zhao <zhaocody@gmail.com>
2021-11-16 19:52:03 +1300

    feat: tree - populating next right pointers in each node

---
+++ leetcode/tree/0116 populating-next-right-pointers-node/connect.js
@@ -0,0 +1,29 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+var connect = function (root) {
+  let queue = [root];
+  while (queue.length && root !== null) {
+    let length = queue.length;
+    for (let i = 0; i < length; i++) {
+      const node = queue.shift();
+      if (i !== length - 1) {
+        node.next = queue[0];
+      }
+      node.left && queue.push(node.left);
+      node.right && queue.push(node.right);
+    }
+  }
+  return root;
+};
