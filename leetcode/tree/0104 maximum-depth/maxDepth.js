c482f816ddb92591a1a8af1d0fa2e026369c4823
Cody Zhao <zhaocody@gmail.com>
2021-11-18 19:40:44 +1300

    feat: tree - maximun depth

--- leetcode/tree/0104 maximum-depth/maxDepth.js
+++ leetcode/tree/0104 maximum-depth/maxDepth.js
@@ -10,9 +10,32 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var maxDepth = function (root) {
+var maxDepth1 = function (root) {
   if (root === undefined || root === null) {
     return 0;
   }
   return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+};
+
+var maxDepth = function (root) {
+  if (!root) {
+    return 0;
+  }
+
+  const queue = [root];
+  let counter = 0;
+
+  while (queue.length) {
+    let length = queue.length;
+    for (let i = 0; i < length; i++) {
+      let node = queue.shift();
+      if (i === length - 1) {
+        counter += 1;
+      }
+      node.left && queue.push(node.left);
+      node.right && queue.push(node.right);
+    }
+  }
+
+  return counter;
 };
