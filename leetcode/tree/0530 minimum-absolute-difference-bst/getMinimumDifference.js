bddd9c6102a9935e7f91f35910e81c944bb2a0b2
Cody Zhao <zhaocody@gmail.com>
2021-12-11 20:36:28 +1300

    feat: tree - minimum absolute difference in bst

--- leetcode/tree/0530 minimum-absolute-difference-bst/getMinimumDifference.js
+++ leetcode/tree/0530 minimum-absolute-difference-bst/getMinimumDifference.js
@@ -25,3 +25,45 @@ var getMinimumDifference = function (root) {
   inorder(root);
   return res;
 };
+
+var getMinimumDifference = function (root) {
+  let arr = [];
+  const inorder = (node) => {
+    if (!node) return;
+    inorder(node.left);
+    arr.push(node.val);
+    inorder(node.right);
+  };
+  inorder(root);
+  let diff = Infinity;
+  for (let i = 1; i < arr.length; i++) {
+    const res = arr[i] - arr[i - 1];
+    if (res < diff) {
+      diff = res;
+    }
+  }
+  return diff;
+};
+
+var getMinimumDifference = function (root) {
+  if (!root) return;
+
+  const stack = [];
+  let cur = root;
+  let res = Infinity;
+  let pre = null;
+
+  while (cur || stack.length) {
+    if (cur) {
+      stack.push(cur);
+      cur = cur.left;
+    } else {
+      cur = stack.pop();
+      if (pre) res = Math.min(res, cur.val - pre.val);
+      pre = cur;
+      cur = cur.right;
+    }
+  }
+
+  return res;
+};
