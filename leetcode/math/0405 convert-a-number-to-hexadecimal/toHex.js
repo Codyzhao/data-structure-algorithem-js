1733f8e7744e122fb124e8ab3517891889517343
Cody Zhao <zhaocody@gmail.com>
2021-12-18 20:02:14 +1300

    feat: math - covert a number to hexadecimal

---
+++ leetcode/math/0405 convert-a-number-to-hexadecimal/toHex.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var toHex = function (num) {
+  var arr = [
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+  ];
+
+  if (num === 0) return Hex[0];
+
+  let res = '';
+
+  if (num < 0) {
+    num += Math.pow(2, 32);
+  }
+
+  while (num > 0) {
+    let rem = num % 16;
+    res = arr[rem] + res;
+    num = Math.floor(num / 16);
+  }
+  return res;
+};
