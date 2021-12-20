df5b51d4a8866721d9551993907951653451a4a0
Cody Zhao <zhaocody@gmail.com>
2021-12-20 20:15:56 +1300

    feat: tree - delete a node in bst

--- leetcode/tree/0450 delete-node-bst/deleteNode.js
+++ leetcode/tree/0450 delete-node-bst/deleteNode.js
@@ -44,3 +44,28 @@ var deleteNode = function (root, key) {
   }
   return root;
 };
+
+var deleteNode = function (root, key) {
+  if (!root) return root;
+  if (root.val === key) {
+    if (!root.left) {
+      return root.right;
+    } else if (!root.right) {
+      return root.left;
+    } else {
+      let cur = root.right;
+      while (cur.left) {
+        cur = cur.left;
+      }
+      cur.left = root.left;
+      return root.right;
+    }
+  }
+  if (root.val > key) {
+    root.left = deleteNode(root.left, key);
+  }
+  if (root.val < key) {
+    root.right = deleteNode(root.right, key);
+  }
+  return root;
+};
