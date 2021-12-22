6d56f91fd5c9dccdb640821f4b9ee4cfd64e3fb5
Cody Zhao <zhaocody@gmail.com>
2021-12-22 20:26:27 +1300

    feat: tree - trim a bst

--- leetcode/tree/0669 trim-a-bst/trimBST.js
+++ leetcode/tree/0669 trim-a-bst/trimBST.js
@@ -29,3 +29,39 @@ var trimBST = function (root, low, high) {
 
   return root;
 };
+
+var trimBST = function (root, low, high) {
+  if (!root) return null;
+
+  // 处理头结点，让root移动到[L, R] 范围内，注意是左闭右闭
+  while (root && (root.val < low || root.val > high)) {
+    if (root.val < low) {
+      root = root.right;
+    } else {
+      root = root.left;
+    }
+  }
+
+  // 此时root已经在[L, R] 范围内
+  let cur = root;
+
+  // 处理左孩子元素小于L的情况
+  while (cur) {
+    while (cur.left && cur.left.val < low) {
+      cur.left = cur.left.right;
+    }
+    cur = cur.left;
+  }
+
+  //重置下角标
+  cur = root;
+  // 此时root已经在[L, R] 范围内，处理右孩子大于R的情况
+  while (cur) {
+    while (cur.right && cur.right.val > high) {
+      cur.right = cur.right.left;
+    }
+    cur = cur.right;
+  }
+
+  return root;
+};
